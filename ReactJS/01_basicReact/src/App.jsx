86c5a3369284dbcd9be908234f6740d84451e194
Ankit Kumar <ankit.kumarrrrr6055@gmail.com>
2025-10-19 02:42:12 +0530

    Done with Components

--- ReactJS/01_basicReact/src/App.jsx
+++ ReactJS/01_basicReact/src/App.jsx
@@ -2,11 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Chai from "./Chai"
 
 function App() {
 
   return (
-    <h1>Chai Aur React</h1>
+    <div>
+      <h1>Chai Aur React</h1>
+      <Chai />
+    </div>
+    
   )
 }
 
