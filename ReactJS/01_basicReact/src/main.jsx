ff46a203df7dfaaef2d83259529d612d2d133cc4
Ankit Kumar <ankit.kumarrrrr6055@gmail.com>
2025-10-20 03:03:37 +0530

    Build Custom React

--- ReactJS/01_basicReact/src/main.jsx
+++ ReactJS/01_basicReact/src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
+import { StrictMode, React } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App</h1>
+    </div>
+  )
+}
+
+const anotherElement = (
+  <a href='https://google.com' target='_blank'>Visit Google</a>
+)
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'Click me to visit google'
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    reactElement
 )
